0c3b214365fc250d7df74d2601d5ed2465f4093b
Qasimtalkin <abulqasim110@gmail.com>
2019-04-07 16:57:09 -0400

    preprocess abstarct

---
+++ Model/Pre_Processing/corpusPP.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from collections import namedtuple
+
+# Abstract Base Classes (ABCs). 
+# Both our corpus will override corpus_preprocess abstract method
+#we will use namedtuple() Factory Function for Tuples with Named Fields
+#Named tuples assign meaning to each position in a tuple and allow
+#for more readable, self-documenting code. They can be used wherever 
+#regular tuples are used, and they add the ability to access fields by name instead 
+#of position index.
+#Documents.namedtuple(typename, field_names, *, rename=False, defaults=None, module=None)
+
+class CorpusPP(ABC):
+    def __init__(self):
+        self.Document = namedtuple(
+            "Document", "doc_id title fulltext snippet topic")
+        self.list_Documents = []
+        super().__init__()
+    @abstractmethod
+    def corpus_pp(self):
+        pass
